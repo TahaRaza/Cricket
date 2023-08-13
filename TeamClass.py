04d345368f48d29f01fd3049b87961c0b2fa4ea2
TahaHRaza <81546850+TahaHRaza@users.noreply.github.com>
2023-08-13 14:18:35 +0500

    openpyxl
    Used openpyxl to import data from MS Excel to PyCharm IDE

--- TeamClass.py
+++ TeamClass.py
@@ -3,11 +3,12 @@ from BallerClass import Baller
 from BallerClass import Batter
 
 # Load the workbook
-global first_name, last_name, age, runs, balls_played, is_on_strike, is_batting, wickets, runs_given,\
+global first_name, last_name, age, runs, balls_played, is_on_strike, is_batting, wickets, runs_given, \
     is_balling, no_of_balls
 wb = load_workbook('Pakistan CT.xlsx')
 # accessing first sheet by its name
 ws = wb['Bat']
+
 
 # Just made a team class for later
 class Team:
